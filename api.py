2646e6c8a9087263db76872c499874f676ed71af
Ken Bailey <kgbailey92@gmail.com>
2018-04-23 12:13:10 -0500

    new api endpoint

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-import urllib.request
+from urllib import request, parse
 import configparser
 
 config = configparser.ConfigParser()
@@ -12,6 +12,13 @@ def test():
     # try test endpoint of current api
     req = urllib.request.urlopen(TEST_URL)
     print(req.read().decode('utf8'))
+
+def sendJsonToUrl(url, json):
+    # send json to urli
+    header={'content-type': 'application/json'}
+    data = parse.urlencode(json).encode()
+    req = request.Request(url=url, data=data, headers=header)
+    response = urllib.request.urlopen(req)
 
 if __name__ == "__main__":
     
